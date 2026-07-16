4df2a2f5fb60e3f1d6aa29b47d5aeeb1485bf36e
GLI2025 <finance.brandon.glass@outlook.com>
2026-07-16 17:38:57 -0600

    Add Guardrails page route to App component

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,11 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 import ExportSuccess from "./pages/ExportSuccess";
+import Guardrails from "./pages/Guardrails";
 
 const queryClient = new QueryClient();
 
@@ -14,11 +16,14 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
+
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
+          <Route path="/guardrails" element={<Guardrails />} />
           <Route path="/export-success" element={<ExportSuccess />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+
+          {/* Keep the catch-all route last */}
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
