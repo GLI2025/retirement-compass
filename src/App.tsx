dea21923fba3044e77b8adf998c857f4fe010f28
GLI2025 <finance.brandon.glass@outlook.com>
2026-07-16 18:34:08 -0600

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,12 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 import ExportSuccess from "./pages/ExportSuccess";
 import Guardrails from "./pages/Guardrails";
+import GuardrailsVolatilityPage from "./pages/GuardrailsVolatilityPage";
+import GuardrailsMethodsPage from "./pages/GuardrailsMethodsPage";
 
 const queryClient = new QueryClient();
 
@@ -16,14 +17,14 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
-          <Route path="/guardrails" element={<Guardrails />} />
           <Route path="/export-success" element={<ExportSuccess />} />
-
-          {/* Keep the catch-all route last */}
+          <Route path="/guardrails" element={<Guardrails />} />
+          <Route path="/guardrails/volatility" element={<GuardrailsVolatilityPage />} />
+          <Route path="/guardrails/methods" element={<GuardrailsMethodsPage />} />
+          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
